5e07c5121dfa274d2ae92da20117cc8b3c008dc5
Puhovon <pv487732@gmail.com>
2022-03-31 14:00:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using Telegram.Bot;
+
+namespace Example_931
+{
+    class Program
+    {
+        static TelegramBotClient bot;
+        static string token = "5281960744:AAFFVPcycBwqmnL1FALgwqS2MPMIVotsN6A";
+
+        static void Main(string[] args)
+        {
+
+
+            #region exc
+
+            //// https://hidemyna.me/ru/proxy-list/?maxtime=250#list
+
+            // Содержит параметры HTTP-прокси для System.Net.WebRequest класса.
+            var proxy = new WebProxy()
+            {
+                Address = new Uri($"http://77.87.240.74:3128"),
+                UseDefaultCredentials = false,
+                //Credentials = new NetworkCredential(userName: "login", password: "password")
+            };
+
+            // Создает экземпляр класса System.Net.Http.HttpClientHandler.
+            var httpClientHandler = new HttpClientHandler() { Proxy = proxy };
+
+            // Предоставляет базовый класс для отправки HTTP-запросов и получения HTTP-ответов 
+            // от ресурса с заданным URI.
+            HttpClient hc = new HttpClient(httpClientHandler);
+
+            bot = new TelegramBotClient(token);
+
+            #endregion
+
+            //bot = new TelegramBotClient(token);
+            bot.OnMessage += MessageListener;
+            bot.StartReceiving();
+            Console.ReadKey();
+        }
+
+        private static void MessageListener(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            string text = $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
+
+            Console.WriteLine($"{text} TypeMessage: {e.Message.Type.ToString()}");
+
+
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
+            {
+                Console.WriteLine(e.Message.Document.FileId);
+                Console.WriteLine(e.Message.Document.FileName);
+                Console.WriteLine(e.Message.Document.FileSize);
+
+                DownloadFile(e.Message.Document.FileId, e.Message.Document.FileName);
+            }
+            else if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Photo)
+            {
+                Console.WriteLine(e.Message.MessageId.ToString());
+                Console.WriteLine(e.Message.Photo);
+                DownloadFile(e.Message.Photo[e.Message.Photo.Length - 1].FileId.ToString(), $"{e.Message.MessageId.ToString()}.jpg");
+            }
+
+            if (e.Message.Text == null) return;
+
+            var messageText = e.Message.Text;
+
+
+            bot.SendTextMessageAsync(e.Message.Chat.Id,
+                $"{messageText}"
+                );
+        }
+
+        private static async void DownloadFile(string fileId, string path)
+        {
+            try
+            {
+                var file = await bot.GetFileAsync(fileId);
+                FileStream fs = new FileStream("_" + path, FileMode.Create);
+                await bot.DownloadFileAsync(file.FilePath, fs);
+                fs.Close();
+
+                fs.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error downloading: " + ex.Message);
+            }
+        }
+    }
+}
